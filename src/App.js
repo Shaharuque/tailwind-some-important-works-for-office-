781bda4c1c1127df9094b1f1b92f4f94d7390384
unknown <shahrukhamin27@gmail.com>
2022-04-06 23:49:22 +0600

    accordian commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Registration from './Components/Registration/Registration';
+import Accordian from './Components/Accordian';
 
 function App() {
   return (
     <div >
       <Registration></Registration>
+      <Accordian></Accordian>
     </div>
   );
 }
